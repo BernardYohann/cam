b9cf292c68f71b0ba039e5709b3a5680dc1d2daf
nexon <nexon@owe>
2016-10-13 10:52:46 +0200

    Ajout de l'autoreload + Exemple d'ORM dans AuthController

---
+++ config/autoreload.js
@@ -0,0 +1,15 @@
+/**
+ * Created by nexon on 13/10/16.
+ */
+
+
+modul.exports.autoreload ={
+    active: true,
+    usePolling: false,
+    dirs: [
+        'api/models',
+        'api/controllers',
+        'api/services'
+    ],
+    ignored: []
+};
