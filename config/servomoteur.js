a8ba0acd5868fb8c0350a7785d452943e48cbfbd
nexon <nexon@owe>
2016-11-18 08:49:40 +0100

    servomoteur for api

---
+++ config/servomoteur.js
@@ -0,0 +1,34 @@
+var express = require('express'),
+    app = express(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server),
+    five = require("johnny-five"),
+    oldAng = 90;
+
+
+server.listen(3000, "127.0.0.1");
+
+board = new five.Board();
+board.on("ready", function() {
+    myServo = new five.Servo(9);
+    board.repl.inject({
+        servo: myServo
+    });
+
+    this.wait(5000, function(){
+        myServo.stop();
+        myServo.to(oldAng);
+        myServo.stop();
+    });
+
+    io.on('connection', function (socket) {
+        socket.on("changeAngle",function(ang){
+            deltAng = oldAng - ang;
+            oldAng = ang;
+            myServo.step(deltAng);
+            console.log('o',oldAng);
+            console.log('a'+ang);
+
+        });
+    });
+});
